1efbf879b7379d7a07669f491559fe0f6b820ac6
ZhaslanR <zhaslan_12@bk.ru>
2017-12-22 16:44:10 +0600

    Добавьте файлы проекта.

---
+++ Dz03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        private static int counter = 0;
+        static void Main(string[] args)
+        {
+            ToDo();
+            Console.ReadLine();
+        }
+        private static void ToDo()
+        {
+            int[] mas = new int[5];
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    mas[i] = counter++;
+                    Console.WriteLine(mas[i]);
+                }
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+            }
+        }
+    }
+}
